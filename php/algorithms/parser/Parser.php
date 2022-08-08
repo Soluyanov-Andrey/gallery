c370ab69da292377bb6a925f3b21efcefb8d3979
АндрейСолуянов <soluyanov.a@yandex.ru>
2022-08-08 09:44:20 +0300

    Разработка Parser4

--- php/algorithms/parser/Parser.php
+++ php/algorithms/parser/Parser.php
@@ -10,7 +10,7 @@ class Parser extends ParentAlgorithms
 {
 
     // Проверяем загрузится ли страница или нет. Если нет то возможно в адрессе ошибка.
-    public static function valid_Url($url_gl): array
+    public static function valid_Url(string $url_gl)
     {
         if (@file_get_contents($url_gl))
         {
@@ -21,118 +21,93 @@ class Parser extends ParentAlgorithms
         {
             $url_gl = Parser::corrective_test_1($url_gl);
         }
-      return $url_gl;
+        return $url_gl;
     }
 
-
     // Если пользователь указал адрес без http:
-    private function corrective_test_1($url_gl): string
+    private function corrective_test_1(string $url_gl)
     {
-        
-        if (strpos($url_gl, 'http') === false) {
-            $url_gl="http://".$url_gl;
-        }
 
+        if (strpos($url_gl, 'http') === false) $url_gl = "http://" . $url_gl;
 
-        if (@file_get_contents($url_gl))
-        {
-            
-            return $url_gl;
-        }
-        else
-        {
-            Parser::corrective_test_2($url_gl);
-        }
+        if (@file_get_contents($url_gl)) return Parser::Search_Img($url_gl);
+
+        return Parser::corrective_test_2($url_gl);
 
     }
     // Если пользователь указал адрес без www:
-    private function corrective_test_2($url_gl): string
+    private function corrective_test_2(string $url_gl)
     {
-        if (strpos($url_gl, 'www') === false) {
-            $url_gl="www://".$url_gl;
-        }
 
+        if (strpos($url_gl, 'www') === false) $url_gl = "www://" . $url_gl;
 
-        if (@file_get_contents($url_gl))
-        {
-            
-            return $url_gl;
-        }
-        else
-        {
-            Parser::corrective_test_3($url_gl);
-        }
+        if (@file_get_contents($url_gl)) return Parser::Search_Img($url_gl);
+
+        return Parser::corrective_test_3($url_gl);
 
     }
     // Если пользователь указал адрес без https://:
-    private function corrective_test_3($url_gl): string
+    private function corrective_test_3(string $url_gl)
     {
-        if (strpos($url_gl, 'https') === false) {
-            $url_gl="https://".$url_gl;
-        }
+        if (strpos($url_gl, 'https') === false) $url_gl = "https://" . $url_gl;
 
+        if (@file_get_contents($url_gl)) return Parser::Search_Img($url_gl);
 
-        if (@file_get_contents($url_gl))
+        echo ("url не рабочий.");
+        return false;
+
+    }
+    //Ищем в html теги img рисунки
+    private static function Search_Img(string $url_gl):
+        array
         {
-            
-            $images = Parser::Search_Img($url_gl);
+
+            global $gl_massages;
+
+            $images = array();
+
+            $data = file_get_contents($url_gl);
+
+            //находит все img src создавая при это не нужные данные
+            preg_match_all('/(img|src)=("|\')[^"\'>]+/i', $data, $media);
+
+            unset($data);
+            //чистим $media от ненужных данных
+            $data = preg_replace('/(img|src)("|\'|="|=\')(.*)/i', "$3", $media[0]);
+
+            //проверяем по расширению файла картинка ли найдена и формируем массив $images с выбокрой
+            foreach ($data as $url)
+            {
+                $info = pathinfo($url);
+
+                if (isset($info['extension']))
+                {
+                    if (($info['extension'] == 'jpg') || ($info['extension'] == 'jpeg') || ($info['extension'] == 'gif') || ($info['extension'] == 'png')
+                    /*$info['extension'] == 'bmp'};*/
+                    //если php 7> можно добавить
+                    )
+                    {
+                        array_push($images, $url);
+                    }
+                }
+            };
+
+            if (empty($images))
+            {
+                echo ("Рисунков не найденно");
+
+            }
+            else
+            {
+                $vr = count($images) + 1;
+                echo ("Рисунки найдены " . $vr . "-шт");
+
+            }
+
             return $images;
-        }
-        else
-        { 
-            echo("url не рабочий.");
-            return false;
         }
 
     }
-     //Ищем в html теги img рисунки
-     private static function Search_Img($url_gl): array
-     {
-         
-         global $gl_massages;
- 
-         $images = array();
- 
-         $data = file_get_contents($url_gl);
- 
-         //находит все img src создавая при это не нужные данные
-         preg_match_all('/(img|src)=("|\')[^"\'>]+/i', $data, $media);
- 
-         unset($data);
-         //чистим $media от ненужных данных
-         $data = preg_replace('/(img|src)("|\'|="|=\')(.*)/i', "$3", $media[0]);
- 
-         //проверяем по расширению файла картинка ли найдена и формируем массив $images с выбокрой
-         foreach ($data as $url) {
-             $info = pathinfo($url);
- 
-             if (isset($info['extension'])) {
-                 if (($info['extension'] == 'jpg') ||
-                     ($info['extension'] == 'jpeg') ||
-                     ($info['extension'] == 'gif') ||
-                     ($info['extension'] == 'png')
-                     /*$info['extension'] == 'bmp'};*/ //если php 7> можно добавить
-                 ) {
-                     array_push($images, $url);
-                 }
-             }
-         };
- 
-         if (empty($images)) {
-             echo("Рисунков не найденно");
-             
- 
-         }else{
-             $vr=count($images)+1;
-             echo("Рисунки найдены ".$vr."-шт");
-            
- 
-         }
- 
-         return $images;
-     }
-
-}
 
 
 
