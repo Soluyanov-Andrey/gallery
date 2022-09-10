0adc5e0400dcecb768ed09ce5f922b6a348b50f6
АндрейСолуянов <soluyanov.a@yandex.ru>
2022-09-10 09:00:53 +0300

    refact

--- php/controller/EngineController.php
+++ php/controller/EngineController.php
@@ -4,6 +4,15 @@
  * Ключевой контролер, откуда начинается запуск.
  *
  */
+ define("URL_CLASS_ALGORITMS","algorithms/");
+ define("DOCUMENT_ROOT",$_SERVER['DOCUMENT_ROOT']."php/");
+
+ define("URL_CLASS_ALGORITMS","algorithms/");
+ define("URL_CLASS_PARSER","algorithms/parser/");
+ define("URL_CLASS_SAVE_FILE","algorithms/seve_file/");
+ define("URL_CLASS_CONTROLLER","controller/");
+
+
 
 // Cоздаем ауто лоадер классов.
 class EngineController
@@ -21,13 +30,13 @@ class EngineController
         
         $algorithms = DOCUMENT_ROOT.URL_CLASS_ALGORITMS.$className.'.php';
         $controller = DOCUMENT_ROOT.URL_CLASS_CONTROLLER.$className.'.php';
-        $parser = DOCUMENT_ROOT.URL_CLASS_PARSER.$className.'.php';
-        
+        $parser = DOCUMENT_ROOT.URL_CLASS_SAVE_FILE.$className.'.php';
+        $seve_file = DOCUMENT_ROOT.URL_CLASS_PARSER.$className.'.php';
         
          (is_file($algorithms)) ? require_once($algorithms): false;
          (is_file($controller)) ? require_once($controller): false;
          (is_file($parser)) ? require_once($parser): false;
-         
+         (is_file($seve_file)) ? require_once($seve_file): false;
     }
 
 }
--- php/init.php
+++ php/init.php
@@ -2,11 +2,4 @@
  
  defined('TEST_MODE') or define('TEST_MODE', false);
 
- define("URL_CLASS_ALGORITMS","algorithms/");
- define("DOCUMENT_ROOT",$_SERVER['DOCUMENT_ROOT']."php/");
-
- define("URL_CLASS_ALGORITMS","algorithms/");
- define("URL_CLASS_PARSER","algorithms/parser/");
- define("URL_CLASS_CONTROLLER","controller/");
- define("DEVELOPER_CLASS_CONTROLLER","developer_scripts/");
- define("TEST","developer_scripts/test/");
+ 
