f07476a6c5a4d06bd9d571f495c0daff446ac072
АндрейСолуянов <soluyanov.a@yandex.ru>
2022-09-10 08:53:40 +0300

    refact

--- php/algorithms/seve_file/SaveFile.php
+++ php/algorithms/seve_file/SaveFile.php
@@ -1,1 +1,19 @@
 <?php
+  class SaveFile extends ParentAlgorithms
+        {
+            public static function seve_fales($url){
+                global $gl_name_img;
+                $path_parts = pathinfo($url);
+
+
+                //echo $path_parts['basename'], "\n";
+                //echo $path_parts['extension'], "\n";
+
+                if (!@copy($url, DIR_TEMP.NAME_IMG.$path_parts['extension'])) {
+                   
+                }
+                $gl_name_img=DIR_TEMP.NAME_IMG.$path_parts['extension'];
+
+            }
+
+        }
--- php/controller/EngineController.php
+++ php/controller/EngineController.php
@@ -22,14 +22,12 @@ class EngineController
         $algorithms = DOCUMENT_ROOT.URL_CLASS_ALGORITMS.$className.'.php';
         $controller = DOCUMENT_ROOT.URL_CLASS_CONTROLLER.$className.'.php';
         $parser = DOCUMENT_ROOT.URL_CLASS_PARSER.$className.'.php';
-        $developer_controller = DOCUMENT_ROOT.DEVELOPER_CLASS_CONTROLLER.$className.'.php';
-        $test = DOCUMENT_ROOT.TEST.$className.'.php';
+        
         
          (is_file($algorithms)) ? require_once($algorithms): false;
          (is_file($controller)) ? require_once($controller): false;
          (is_file($parser)) ? require_once($parser): false;
-         (is_file($developer_controller)) ? require_once($developer_controller): false;
-         (is_file($test)) ? require_once($test): false;
+         
     }
 
 }
