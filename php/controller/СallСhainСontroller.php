d32d96386d8c811e6cb90969d37a46bca5012b1a
АндрейСолуянов <soluyanov.a@yandex.ru>
2022-10-04 12:54:22 +0300

    watermark

--- php/controller/СallСhainСontroller.php
+++ php/controller/СallСhainСontroller.php
@@ -6,7 +6,6 @@
  *
  */
 
-
 class СallСhainСontroller
 {
 
@@ -16,97 +15,99 @@ class СallСhainСontroller
     private $saveTestFileExtension;
     private $saveNewNameMD5;
     private $saveColor;
-    
 
-    
-    public function usageGetImage($value, $saitUrl, $width, $higth){
+    public function usageGetImage($value, $saitUrl, $width, $higth)
+    {
 
-        $this->saveMessage =  GetImages::getImagesUrl($value, $saitUrl, $width, $higth);
+        $this->saveMessage = GetImages::getImagesUrl($value, $saitUrl, $width, $higth);
         $this->saveImageUrl = $this->saveMessage['data'];
-        
+
         return $this;
     }
-   
-    public function usageSaveFile(){
+
+    public function usageSaveFile()
+    {
 
         //-------------------Идентификаторы для сокращения------------------
         $message = $this->saveMessage;
         $saveImageUrl = $this->saveImageUrl;
-       
-        if($message['result']){
 
-                 $this->saveMessage = SaveFile::seveFalesImagis($saveImageUrl); 
-                 $this->saveTestFileExtension = URL_FOLDER_TMP . NAME_TEMPORARY_FILE . $this->saveMessage ;
-              
+        if ($message['result']) {
+
+            $this->saveMessage = SaveFile::seveFalesImagis($saveImageUrl);
+            $this->saveTestFileExtension = URL_FOLDER_TMP . NAME_TEMPORARY_FILE . $this->saveMessage;
+
         }
-        
+
         return $this;
     }
 
-    public function usageImageResize(){
+    public function usageImageResize()
+    {
 
         //-------------------Идентификаторы для сокращения------------------
         $message = $this->saveMessage;
         $saveImageUrl = $this->saveImageUrl;
-        
-        if($message['result']){
+
+        if ($message['result']) {
 
             ImageResize::resize($saveImageUrl);
-           
-                
+
         }
-        
+
         return $this;
     }
 
-    public function usageReadSaveHashFile(){
-        
+    public function usageReadSaveHashFile()
+    {
+
         //-------------------Идентификаторы для сокращения------------------
         $message = $this->saveMessage;
-        $smollImg = URL_FOLDER_TMP.NAME_SMOLL_IMG;
+        $smollImg = URL_FOLDER_TMP . NAME_SMOLL_IMG;
 
-        if($message['result']){
+        if ($message['result']) {
 
-            $this->saveMessage = ReadSaveHashFile::seveFalesImagis($smollImg); 
+            $this->saveMessage = ReadSaveHashFile::seveFalesImagis($smollImg);
             $this->saveNewNameMD5 = $this->saveMessage['data'];
-            
+
         }
 
         return $this;
     }
-   
 
-    public function usagePredominantColor(){
+    public function usagePredominantColor()
+    {
         //-------------------Идентификаторы для сокращения------------------
-        $smollImg = URL_FOLDER_TMP.NAME_SMOLL_IMG;
+        $smollImg = URL_FOLDER_TMP . NAME_SMOLL_IMG;
         $message = $this->saveMessage;
 
-        if($message['result']){
+        if ($message['result']) {
 
-        $this->saveMessage = PredominantColor::Get_Img($smollImg);
-        $this->saveColor = $this->saveMessage['data'];
+            $this->saveMessage = PredominantColor::Get_Img($smollImg);
+            $this->saveColor = $this->saveMessage['data'];
 
         }
-        
+
         return $this;
     }
 
-    
-
-    public function usageWatermark(){
+    public function usageWatermark()
+    {
         //-------------------Идентификаторы для сокращения------------------
         $color = $this->saveColor;
+        $message = $this->saveMessage;
 
-        WaterMark::to_draw($color);
+        if ($message['result']) {
+
+            WaterMark::to_draw($color);
+        }
         return $this;
     }
 
-    public function usageWorkFileDirectory(){
-        
-        
+    public function usageWorkFileDirectory()
+    {
+
         return $this;
     }
-
-   
 
 }
