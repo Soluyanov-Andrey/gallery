46609aae885133a1ddbbcff3561ef429f715c38c
Soluyanov-Andrey <a--b--s@yandex.ru>
2022-12-03 19:20:02 +0300

    настройки

--- frontendDevReact/src/App.jsx
+++ frontendDevReact/src/App.jsx
@@ -8,26 +8,45 @@ export const App = () =>  {
   return (
   <>
     <div className='top-block'>
-    
        <div className='data'>
 
-        <input  type="text" id="data-search" name="search" placeholder="Url загрузки" />
-        <input  type="submit" id="data-submit" value="Загрузка" />
+        <input  type="text" id="data__search" name="search" placeholder="Url загрузки" />
+        <input  type="submit" id="data__submit" value="Загрузка" />
 
+        <img id="data__bell" src={imageFile} alt="колокол" title="Уведомления" />
+        <img id="data__settings" src={imageFile1} alt="колокол" title="Настройки" />
 
-        <img id="bell" src={imageFile} alt="колокол" title="Уведомления" />
-        <img id="settings" src={imageFile1} alt="колокол" title="Настройки" />
-
-        </div>
-        
-      
+      </div> 
     </div>
+
+
 
     <div className='gallery'>
-
     </div>
+
+    <div className = "blocking-layer"></div>
+
+
+    <div  className="settings-win">
+        <div>
+           <div className="settings-win__close">X</div>
+           <h2>Настройки</h2>
+            
+           <input type="text" id="settings-win__width" name="search" placeholder=""  maxlength="4"/>
+           <span >&nbsp;&nbsp;&nbsp;Мин ширина 100-6000</span>
+           <br></br>
+           <input type="text" id="settings-win__heigth" name="search" placeholder=""  maxlength="4"/>
+           <span>&nbsp;&nbsp;&nbsp;Мин ширина 200-4000</span>  
+           <br></br>
+           <input type="checkbox" name="yourName" id="settings-win__checkbox"/> <>Удалить все картинки из галереи</>
+           <input type="submit" id="settings-win__submit" value="Применить"/>
+        </div>
+      </div>
+    
   </>
   );
+
+
 }
 
 
