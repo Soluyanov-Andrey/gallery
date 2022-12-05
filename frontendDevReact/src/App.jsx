7db42721aa7a1d13d53e064cf46cf57251c4fa72
Soluyanov-Andrey <a--b--s@yandex.ru>
2022-12-05 18:22:14 +0300

    refact

--- frontendDevReact/src/App.jsx
+++ frontendDevReact/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef} from 'react';
 import './App.css';
 import imageFile from './img/bell-static.gif';
 import imageFile1 from './img/settings.jpg';
@@ -7,12 +7,13 @@ import { BlockingLayer } from './component/BlockingLayer';
 import { SettingsWin } from './component/SettingsWin';
 import { WinLog } from './component/WinLog';
 
+
 const handleClick = () => {
     console.log('this это:', this);
   }
 
 export const App = () => {
- 
+ const [show, setCount] = useState(true);
   return (
     <>
       <div className='top-block'>
@@ -21,15 +22,15 @@ export const App = () => {
           <input type="text" id="data__search" name="search" placeholder="Url загрузки" />
           <input type="submit" id="data__submit" value="Загрузка" />
 
-          <img id="data__bell" onClick={handleClick} src={imageFile} alt="колокол" title="Уведомления" />
+          <img id="data__bell" onClick={() => handleClick()} src={imageFile} alt="колокол" title="Уведомления" />
           <img id="data__settings" src={imageFile1} alt="колокол" title="Настройки" />
 
         </div>
       </div>
 
    <Gallery/>
-   <BlockingLayer/>
-   <SettingsWin/>
+   {/* <BlockingLayer/> */}
+   <SettingsWin show ={show}/>
    <WinLog/>
     </>
   );
--- frontendDevReact/src/component/SettingsWin.js
+++ frontendDevReact/src/component/SettingsWin.js
@@ -1,24 +1,31 @@
 import React from 'react';
 //import './App.css';
 
-export const SettingsWin = () => {
+export const SettingsWin = ({show}) => {
+
+//   const DataCap = {
+//     display: "none"
+// }
+
+
   return (
-    <div className="settings-win">
-    <div>
-      <div className="settings-win__close">X</div>
-      <h2>Настройки</h2>
+   
+    <div className="settings-win" style={show?{display: "none"}:{display: "block"}} >
+        <div>
+          <div className="settings-win__close">X</div>
+          <h2>Настройки</h2>
 
-      <input type="text" id="settings-win__width" name="search" placeholder="" maxLength="4" />
-      <span >&nbsp;&nbsp;&nbsp;Мин ширина 100-6000</span>
-      <br></br>
-      <input type="text" id="settings-win__heigth" name="search" placeholder="" maxLength="4" />
-      <span>&nbsp;&nbsp;&nbsp;Мин ширина 200-4000</span>
-      <br></br>
+          <input type="text" id="settings-win__width" name="search" placeholder="" maxLength="4" />
+          <span >&nbsp;&nbsp;&nbsp;Мин ширина 100-6000</span>
+          <br></br>
+          <input type="text" id="settings-win__heigth" name="search" placeholder="" maxLength="4" />
+          <span>&nbsp;&nbsp;&nbsp;Мин ширина 200-4000</span>
+          <br></br>
 
-      <input type="checkbox" name="yourName" className="settings-win__checkbox" /> <>Удалить все картинки из галереи</>
-      <input type="submit" className="settings-win__submit" value="Применить" />
+          <input type="checkbox" name="yourName" className="settings-win__checkbox" /> <>Удалить все картинки из галереи</>
+          <input type="submit" className="settings-win__submit" value="Применить" />
+        </div>
     </div>
-  </div>
   );
 
 
